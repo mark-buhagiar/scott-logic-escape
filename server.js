ca27f0c8415bf278dbfd3b2ede539744360981da
Mark Buhagiar <mbuhagiar@scottlogic.com>
2020-11-25 12:19:26 +0000

    Port fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function(req,res){
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-var port = 8080;
+var port = (process.env.PORT || '8080');
 app.listen(port, function(){
   console.log('Express server listening on port ' + port);
 })
