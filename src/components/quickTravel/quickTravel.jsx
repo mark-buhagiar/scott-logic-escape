1f54fcaf8d21c1f65c2d5c5d3aca283da809d849
Mark Buhagiar <mbuhagiar@scottlogic.com>
2020-11-27 14:03:54 +0000

    Quality of life improvements

--- src/components/quickTravel/quickTravel.jsx
+++ src/components/quickTravel/quickTravel.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Paper from "@material-ui/core/Paper";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+import "./quickTravel.scss";
 
 const QuickTravel = () => {
   const [destinations, setDestinations] = useState([]);
+  const [collapsed, setCollapsed] = useState(false);
 
   useEffect(() => {
     function updateDestinations() {
@@ -19,18 +24,45 @@ const QuickTravel = () => {
 
   const renderDestinations = () => {
     return (
-      <Paper className="panel" elevation={1}>
-        <h3>Quick Travel Links</h3>
-        {destinations.map(({ key, name }) => (
-          <Link key={key} to={`/room/${key}`}>
-            {name}
-          </Link>
-        ))}
+      <Paper id="Quick-Travel" className="panel" elevation={0}>
+        <div className="title">
+          <h3>Quick Travel Links</h3>
+          <ArrowForwardIosIcon
+            className="clickable"
+            onClick={() => setCollapsed(true)}
+          />
+        </div>
+        <ul>
+          {destinations.map(({ key, name }) => (
+            <li key={key}>
+              <Link to={`/room/${key}`}>{name}</Link>
+            </li>
+          ))}
+        </ul>
       </Paper>
     );
   };
 
-  return destinations.length > 0 ? renderDestinations() : <></>;
+  const renderCollapsed = () => {
+    return (
+      <Paper id="Quick-Travel" className="panel collapsed" elevation={0}>
+        <ArrowBackIosIcon
+          className="clickable"
+          onClick={() => setCollapsed(false)}
+        />
+      </Paper>
+    );
+  };
+
+  return destinations.length > 0 ? (
+    collapsed ? (
+      renderCollapsed()
+    ) : (
+      renderDestinations()
+    )
+  ) : (
+    <></>
+  );
 };
 
 export default QuickTravel;
--- src/components/sections/image.jsx
+++ src/components/sections/image.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import Modal from "@material-ui/core/Modal";
+import Paper from "@material-ui/core/Paper";
 
 const ImageSection = ({ label, source }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <label>{label}</label>
-      <img src={source} alt={label} />
+      <img
+        className="zoomable preview"
+        src={source}
+        alt={label}
+        onClick={handleOpen}
+      />
+      <Modal open={open} onClose={handleClose}>
+        
+          <Paper className="panel modal" elevation={3}>
+            <img src={source} alt={label} />
+          </Paper>
+        
+      </Modal>
     </>
   );
 };
